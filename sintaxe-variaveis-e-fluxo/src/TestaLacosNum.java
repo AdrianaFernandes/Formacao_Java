10a7a7709ca084dd64ac058bdc6bf0264011382f
Adriana Fernandes <adrianafernandes.af@gmail.com>
2020-03-04 08:56:29 -0300

    sintaxe variaveis e fluxo - Alura

---
+++ sintaxe-variaveis-e-fluxo/src/TestaLacosNum.java
@@ -0,0 +1,17 @@
+
+public class TestaLacosNum {
+
+	public static void main(String[] args) {
+		
+		for(int linha = 0; linha <=5; linha++){
+			
+			for (int coluna = 0 ; coluna <= linha; coluna ++) {
+				
+				System.out.print("1");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,27 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double salario = 1250.70;
+		
+		System.out.println("meu salario é " + salario);
+		
+		double idade;
+		idade = 37;
+		
+		System.out.println("minha idade é " + idade);
+		
+		double divisao = 3.14 / 2;
+		
+		System.out.println(divisao);
+		
+		int outradivisao = 5 / 2;
+		
+		System.out.println(outradivisao);
+		
+		double novaTentativa = 5.0 / 2;
+		
+		System.out.println(novaTentativa);
+	}
+}
